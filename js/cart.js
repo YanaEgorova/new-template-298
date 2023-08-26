a347333366c2766b338e9800aba753a7b520b1e3
Yana <yanaegorova69.3@gmail.com>
2023-08-26 03:06:56 +0300

    done

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,8 @@ import { products } from './data/products.js';
 import { item } from './templates/product-item.js';
 
 const productsList = document.querySelector('.js_products__list');
-
+let counter = 0;
 products.forEach(product => {
-    productsList.insertAdjacentHTML('beforeend',item(product));
-})
+  counter += 1;
+  productsList.insertAdjacentHTML('beforeend', item(product, counter));
+});
--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -1,29 +1,36 @@
-export const item = product => {
+export const item = (product, counter) => {
   return `
   <li class="item">
   <div class="product__block js_product glass glass-m" id="${product.id}">
-     <div class="product__block--inner">
-    <div class="product__img-box">
-      <img src="${product.image}" alt="" class="img" />
+    <div class="price-block">
+    <div class="price-block--inner">
+      <div class="price-block-shadow"></div>
+      <span class="product__price">$${product.price.toFixed(2)}</span>
+      <span class="product__index">${counter <= 9 ? '0' + counter : counter}</span>
+      </div>
     </div>
-
-    <div class="product__text-box">
-      <p class="product__name js_product-name">${product.name}</p>
-      ${
-        product.description[0].text.length > 0
-          ? '<p class="text product__desc">' + product.description[0].text.slice(0, 80) + '...' + '</p>'
-          : ''
-      }
-      <span class="product__price">
-        $${product.price.toFixed(2)}
-      </span>
-    </div>
-    </div>
-    <div class="product__btn-block">
-      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${
+    <div class="product__block--inner">
+      <div class="product__img-box">
+        <img src="${product.image}" alt="" class="img" />
+      </div>
+      <div class="product__text-box">
+        <p class="product__name js_product-name"><span class="product__btn-arrow"></span> ${product.name}</p>
+        ${
+          product.description[0].text.length > 0
+            ? '<p class="text product__desc">' + product.description[0].text.slice(0, 80) + '...' + '</p>'
+            : ''
+        }
+        <div class="product__btn-block">
+          <a href="product-page.html?id=${product.id}" class="product__btn btn--first" style="margin: ${
     product.type == 'ring' || product.type == 'clothing' ? 'auto' : ''
-  }">details</a>
-      ${product.type == 'ring' || product.type == 'clothing' ? '' : '<button class="btn js_add-to-cart">buy</button>'}
+  }">details <span class="product__btn-arrow"></span></a>
+          ${
+            product.type == 'ring' || product.type == 'clothing'
+              ? ''
+              : '<button class="product__btn js_add-to-cart">buy <span class="product__btn-arrow"></span></button>'
+          }
+        </div>
+      </div>
     </div>
   </div>
 </li>
